0ca20813a00eae85f753bb15e54e8a5d1f99ad79
Ezequiel <ezequielgiribarren@gmail.com>
2024-05-30 16:54:40 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Define la raíz del proyecto
-  root: 'src',
-  // Define la carpeta de salida
-  build: {
-    outDir: '../dist',
-  },
-  // Configuración del servidor de desarrollo
-  server: {
-    port: 3000, // Puerto del servidor de desarrollo
-    open: true, // Abre el navegador automáticamente
-    proxy: {
-      // Configuración de proxy si se necesita redirigir ciertas solicitudes
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  // Plugins
-  plugins: [
-    // Aquí puedes agregar plugins de Vite
-  ],
-});
+  plugins: [react()],
+})
