40633fe6dbe43d674917b8084778c08f8bb5003a
Ezequiel <ezequielgiribarren@gmail.com>
2024-05-30 16:48:40 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,28 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // Define la raíz del proyecto
+  root: 'src',
+  // Define la carpeta de salida
+  build: {
+    outDir: '../dist',
+  },
+  // Configuración del servidor de desarrollo
   server: {
+    port: 3000, // Puerto del servidor de desarrollo
+    open: true, // Abre el navegador automáticamente
     proxy: {
+      // Configuración de proxy si se necesita redirigir ciertas solicitudes
       '/api': {
-        target: 'http://localhost:5000', // Aquí coloca la URL de tu servidor Node.js
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Elimina el prefijo '/api' en las solicitudes
+        secure: false,
       },
     },
   },
+  // Plugins
+  plugins: [
+    // Aquí puedes agregar plugins de Vite
+  ],
 });
-
-
-
-// // vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {    
-//       '/api': {
-//         target: 'https://www.tclarevista.com.ar', // Sin necesidad de especificar un puerto para HTTPS
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//     },
-//   },
-// });
