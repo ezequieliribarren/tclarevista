dd33228ddc2d871e4ffe68163577bd86ef467401
Ezequiel <ezequielgiribarren@gmail.com>
2024-05-30 17:15:27 -0300

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],  // Plugin para soporte de React
-  server: {
-    port: 3000,         // Puerto del servidor de desarrollo
-    open: true,         // Abre el navegador automáticamente al iniciar el servidor
-  },
-  build: {
-    outDir: 'dist',     // Carpeta de salida para la compilación
-    sourcemap: true,    // Genera archivos sourcemap para depuración
-  },
-  resolve: {
-    alias: {
-      '@': '/src',      // Alias para facilitar las importaciones desde 'src'
-    },
-  },
-});
+  plugins: [react()],
+})
