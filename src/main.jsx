494818fd985caad81b8582f9840c3147e898bf88
Ezequiel <ezequielgiribarren@gmail.com>
2024-06-19 08:57:45 -0300

    vite

--- src/main.jsx
+++ src/main.jsx
@@ -282,5 +282,5 @@ const router = createHashRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
